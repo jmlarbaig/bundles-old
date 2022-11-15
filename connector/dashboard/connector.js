234050a921eb847b1a63b6160e50ac06f4b3ac7c
Jean marie Larbaig <jeanmarie.larbaig@gmail.com>
2022-11-15 13:02:13 +0100

    Revert "LED pour adresse ip, ntp et mqtt, reconnection mqtt et changement d'adresse automatique au changement de réseau"
    This reverts commit 040cac8c73769861229309abc49836cdc111240b.

--- connector/dashboard/connector.js
+++ connector/dashboard/connector.js
@@ -325,6 +325,11 @@ var liste_donnees = {'Team':[], 'Individual':[]}
 
             nodecg.sendMessage('dataOverwrite', dataNewConfig);
 
+            getLocalIP().then((ipAddr) => {
+                console.log(ipAddr); // 192.168.0.122
+                ipAddress.value = ipAddr
+            });
+
         }
         catch(e){
             console.log(e)
@@ -370,54 +375,63 @@ var liste_donnees = {'Team':[], 'Individual':[]}
         document.getElementById('eventId').value = value.eventId.toString()
     })
 
-    ipAddress.on('change', (newValue, oldValue) => {
-        let ipHtml = $('#ipAdresse');
-        ipHtml.text(newValue)
-        console.log(newValue.includes('10.'))
-        if(newValue != undefined && newValue.includes('10.')){
-            $('#led_ipAdresse').addClass('led-green')
-            $('#led_ipAdresse').removeClass('led-red')
-            $('#led_ipAdresse').removeClass('led-orange')
-        }else if(newValue != undefined){
-            $('#led_ipAdresse').addClass('led-orange')
-            $('#led_ipAdresse').removeClass('led-red')
-            $('#led_ipAdresse').removeClass('led-green')
-        }else{
-            $('#led_ipAdresse').removeClass('led-green')
-            $('#led_ipAdresse').removeClass('led-red')
-            $('#led_ipAdresse').addClass('led-orange')
-        }
-    })
 
-    nowNtp.on('change', (newValue, oldValue)=>{
-        if(!newValue.includes('Timeout waiting for NTP')){
-            $('#ntpTime').text(newValue)
-            $('#led_NTP').addClass('led-green')
-            $('#led_NTP').removeClass('led-orange')
-            $('#led_NTP').removeClass('led-red')
-        }
-        else{
-            $('#ntpTime').text(newValue)
-            $('#led_NTP').removeClass('led-red')
-            $('#led_NTP').addClass('led-orange')
-            $('#led_NTP').removeClass('led-green')
-        }
-    })
-
-    Mqtt_connected.on('change', (newValue, oldValue) => {
-        console.log("NewValue MQTT =",newValue)
-        if(newValue.connected == true){
-            $('#mqttMSG').text('Connected')
-            $('#led_MQTT').addClass('led-green')
-            $('#led_MQTT').removeClass('led-red')
-        }
-        else{
-            if(newValue.error != ''){
-                $('#mqttMSG').text('Reconnection ...')
-            }else{
-                $('#mqttMSG').text('Disconnected')
-            }
-            $('#led_MQTT').addClass('led-red')
-            $('#led_MQTT').removeClass('led-green')
-        }
-    })
+/**
+ * Get Local IP Address
+ * 
+ * @returns Promise Object
+ *
+ * getLocalIP().then((ipAddr) => {
+ *    console.log(ipAddr); // 192.168.0.122
+ * });
+ */
+ function getLocalIP() {
+    return new Promise(function(resolve, reject) {
+      // NOTE: window.RTCPeerConnection is "not a constructor" in FF22/23
+      var RTCPeerConnection = /*window.RTCPeerConnection ||*/ window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
+  
+      if (!RTCPeerConnection) {
+        reject('Your browser does not support this API');
+      }
+      
+      var rtc = new RTCPeerConnection({iceServers:[]});
+      var addrs = {};
+      addrs["0.0.0.0"] = false;
+      
+      function grepSDP(sdp) {
+          var hosts = [];
+          var finalIP = '';
+          sdp.split('\r\n').forEach(function (line) { // c.f. http://tools.ietf.org/html/rfc4566#page-39
+              if (~line.indexOf("a=candidate")) {     // http://tools.ietf.org/html/rfc4566#section-5.13
+                  var parts = line.split(' '),        // http://tools.ietf.org/html/rfc5245#section-15.1
+                      addr = parts[4],
+                      type = parts[7];
+                  if (type === 'host') {
+                      finalIP = addr;
+                  }
+              } else if (~line.indexOf("c=")) {       // http://tools.ietf.org/html/rfc4566#section-5.7
+                  var parts = line.split(' '),
+                      addr = parts[2];
+                  finalIP = addr;
+              }
+          });
+          return finalIP;
+      }
+      
+      if (1 || window.mozRTCPeerConnection) {      // FF [and now Chrome!] needs a channel/stream to proceed
+          rtc.createDataChannel('', {reliable:false});
+      };
+      
+      rtc.onicecandidate = function (evt) {
+          // convert the candidate to SDP so we can run it through our general parser
+          // see https://twitter.com/lancestout/status/525796175425720320 for details
+          if (evt.candidate) {
+            var addr = grepSDP("a="+evt.candidate.candidate);
+            resolve(addr);
+          }
+      };
+      rtc.createOffer(function (offerDesc) {
+          rtc.setLocalDescription(offerDesc);
+      }, function (e) { console.warn("offer failed", e); });
+    });
+  }
